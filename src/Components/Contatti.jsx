aabac94204148c2ac94cb32ee17deb31e5827392
Andrea Lauro <125922974+Andreala94@users.noreply.github.com>
2025-11-26 20:51:42 +0100

    Initial React project

--- src/Components/Contatti.jsx
+++ src/Components/Contatti.jsx
@@ -1,11 +1,54 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope, faShop, faAddressBook, faCircleInfo, faLocationDot, faClock }from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import Maps from "./Maps"
 
 function Contatti() {
   return (
-    <div className="contatti-page">
-      <h1>Contattaci</h1>
-      <p>Qui puoi inserire un form di contatto o le info aziendali.</p>
+    <>
+    <div className="contatti-page text-center mt-5">
+  <h1><FontAwesomeIcon icon={faCircleInfo} />Informazioni</h1>
+</div>
+
+<div className="d-flex flex-column flex-md-row justify-content-center gap-4 mt-4">
+  {/* Sezione Negozio */}
+  <div className="negozio p-3 bg-light rounded shadow text-start w-100 w-md-auto">
+    <h2><FontAwesomeIcon icon={faShop} />{" "}Negozio</h2>
+    <br />
+    <h5> <FontAwesomeIcon icon={faLocationDot} />{" "}Via Leone XIII, 9, 74015 Martina Franca TA</h5>    
+    <br />
+    <h4><FontAwesomeIcon icon={faClock} />{" "}Orari Apertura:</h4>
+    <br />
+     <h5>Lunedì – Venerdì 9:00- 13:00 / 17:00 – 20:30</h5>
+    
+    <h5><h5>Sabato 9:00 – 13:00 / Pomeriggio Chiuso</h5></h5>
+  </div>
+
+  {/* Sezione Contatti */}
+  <div className="info p-3 bg-light rounded shadow text-start w-100 w-md-auto ">
+
+    <h2><FontAwesomeIcon icon={faAddressBook} />{" "}Contatti</h2>
+    <br />
+    <div className="mb-3">
+      <h4><FontAwesomeIcon icon={faPhone} /> Telefono</h4>
+      <h5>080 2378818</h5>
     </div>
+    <div className="mb-3">
+      <h4><FontAwesomeIcon icon={faWhatsapp} />{" "}WhatsApp</h4>
+      <h5>3287892261</h5>
+    </div>
+    <div>
+      <h4> <FontAwesomeIcon icon={faEnvelope} />{" "}Email</h4>
+      <h5>info@simphonia.com</h5>
+    </div>
+  </div>
+</div>
+
+<div className="mt-5">
+  <Maps />
+</div>
+   
+    </>
   );
 }
 
---
+++ src/Components/Maps.jsx
@@ -0,0 +1,20 @@
+function Maps() {
+  return (
+    <div style={{ width: "100%", height: "400px" }}>
+      <iframe
+        title="SimPhonia Store"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.9196811921656!2d17.33588037549762!3d40.69776653848771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134653c35214f543%3A0xa627d7e5a043bcf2!2sSimPhonia%20Store%20-%20Vodafone%2C%20Tim%2C%20Ho%2C%20Iliad%20Store%20-%20Negozio%20di%20Telefonia!5e0!3m2!1sit!2sit!4v1764022609927!5m2!1sit!2sit"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Maps;
+
+
